260344c69775612099f84e13472a8314976d1f56
Kartikeya Kandpal <kandpalkartikeya93@gmail.com>
2023-06-06 22:45:41 +0530

    print the corresponding pattern for N

---
+++ question_7.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+import javax.print.event.PrintEvent;
+
+
+public class question_7 {
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+
+        int N=sc.nextInt();
+        if(N<1||N>100){
+            System.out.println("Invalid Input");
+
+        }
+        else{
+
+            for(int row=1;row<=N;row++){
+                
+                for(int column=1;column<=row;column++){
+
+                //  if(row%2==0){System.out.print(" ");}
+                    // else{
+                
+                  System.out.print(column +" ");
+                
+                }
+
+
+
+                System.out.println();
+            }
+            
+
+
+
+        }
+
+    }
+    
+}
+
